afa829eee18d24ef31d98aa7edfcd1139a4c3ef6
Lottetekulve <78353514+Lottetekulve@users.noreply.github.com>
2021-03-23 14:11:59 +0100

    ejs files and server

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const path = require('path');
+require('dotenv').config();
+
+const app = express()
+
+
+const PORT = process.env.PORT || 5500;
+
+
+// Tell express to use a 'static/public' folder
+// If the url matches a file it will send that file
+app.use(express.static(path.join(__dirname, "static/public")));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+app.get('/', function (req, res) {
+    res.render('index');
+})
+
+app.get('/shirt', function (req, res) {
+    res.render('shirt');
+})
+
+app.get('/shirt/order', function (req, res) {
+    res.render('order');
+})
+
+app.get('/signup', function (req, res) {
+    res.render('signup');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`App is launched on http://localhost:${PORT}`)
+  });
+  
